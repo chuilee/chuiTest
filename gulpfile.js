6af93510ed11b7b644446585dcfe2cd98119213b
chuilee <chui.lee8@gmail.com>
2015-04-20 17:30:31 +0800

    modified:   gulpfile.js 	modified:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
-
+// 加载模块
 var gulp = require( "gulp" );
 
+var connect = require( "gulp-connect" );
+
+
 // 配置变量
-var html = "./assets/**/*.html";
-var jsSrc = "./assets/js/*.js";
-var lessSrc = "./assets/less/**/*.less";
-var styleSrc = "./assets/less/styles.status.less";
+var html = "./app/**/*.html";
+var jsSrc = "./app/modules/*.js";
+var lessSrc = "./app/assets/less/*.less";
+var styleSrc = "./app/assets/less/styles.status.less";
 
 // 创建服务器
-var connect = require( "gulp-connect" );
 gulp.task( "connect", function() {
 
   connect.server({
-    root: "assets",
-    livereload: true
+    root: "app",
+    livereload: true,
+    port: 8000
+  });
+
+} );
+
+// 创建服务器
+gulp.task( "github", function() {
+
+  connect.server({
+    root: "github",
+    livereload: true,
+    port: 8001
   });
 
 } );
@@ -22,48 +36,23 @@ gulp.task( "connect", function() {
 var less = require( "gulp-less" );
 var minifyCSS = require( "gulp-minify-css" );
 var rename = require( "gulp-rename" );
+var lessPluginCleanCSS = require( "less-plugin-clean-css" );
+var lessPluginAutoprefix = require( "less-plugin-autoprefix" );
+var cleancss = new lessPluginCleanCSS( { advanced: true } );
+var autoprefix = new lessPluginAutoprefix( { browsers: [ "last 2 versions" ] } );
 gulp.task( "less", function(){
-
-  var lessPluginCleanCSS = require( "less-plugin-clean-css" );
-  var lessPluginAutoprefix = require( "less-plugin-autoprefix" );
-  var cleancss = new lessPluginCleanCSS( { advanced: true } );
-  var autoprefix = new lessPluginAutoprefix( { browsers: [ "last 2 versions" ] } );
-
+  
   gulp.src( styleSrc )
     .pipe( less({
       plugins: [ autoprefix ]
     }) )
-    .pipe( gulp.dest( "./assets/css" ) )
+    .pipe( gulp.dest( "./app/assets/css" ) )
     .pipe( minifyCSS() )
     .pipe( rename( { extname: ".min.css" } ) )
-    .pipe( gulp.dest( "./assets/css/" ) );
+    .pipe( gulp.dest( "./app/assets/css/mins/" ) )
+    .pipe( connect.reload() );
 
 } );
-
-// 合并雪碧图
-var spritesmith = require( "gulp.spritesmith" );
-var icons = "./assets/images/icons/*.png";
-gulp.task( "sprite", function(){
-
-  var spriteData = gulp.src( icons )
-                      .pipe( spritesmith({
-                        imgName: "icons.png",
-                        cssName: "icons.less"
-                      }) );
-  spriteData.pipe( gulp.dest( "./built/sprite" ) );
-
-} );
-
-// 压缩js
-var uglyfiy = require( "gulp-uglyfly" );
-gulp.task( "uglyfly", function(){
-
-  gulp.src( jsSrc )
-    .pipe( uglyfiy() )
-    .pipe( rename( { extname: ".min.js" } ) )
-    .pipe( gulp.dest( "./assets/js" ) );
-
-} )
 
 gulp.task( "html", function(){
 
@@ -87,8 +76,7 @@ gulp.task( "watch", function(){
 
   gulp.watch( [ lessSrc ], [ "less" ] );
 
-  // gulp.watch( [ jsSrc ], [ "uglyfly" ] );
-
 } );
 
 gulp.task( "default", [ "connect", "watch" ] );
+
