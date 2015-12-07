0456b62b1599367506bc88ca9c6eb835a1681308
chuilee <chui.lee8@gmail.com>
2015-12-07 18:15:38 +0800

    mask-svg

--- Library/common.js
+++ Library/common.js
@@ -1,4 +1,3 @@
-
 //如果浏览器支持requestAnimFrame则使用requestAnimFrame否则使用setTimeout  
 window.requestAnimFrame = (function() {
     return window.requestAnimationFrame ||
@@ -8,3 +7,16 @@ window.requestAnimFrame = (function() {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
+
+// 寄生组合式继承
+function object(o) {
+    function F() {}
+    F.prototype = o;
+    return new F();
+}
+
+function inheritPrototype(subType, superType) {
+    var prototype = object(superType.prototype);
+    prototype.constructor = subType;
+    subType.prototype = prototype;
+}
