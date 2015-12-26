04b726c5c92c4808e8808ca30021f06061d50f3b
chuilee <chui.lee8@gmail.com>
2015-12-26 17:46:50 +0800

    array

--- XTest/underscore/array.js
+++ XTest/underscore/array.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
-var _ = require("../../../node_modules/underscore");
-
-var _arr = [1,2,3,4,5,6, "chuilee"];
-var _2arr = [[1,2],[3,4],[6,5]];
-var _oarr = [{name: "CHUI"}, {name: "LEE"}, {name: "CHUI", age: "20"}, {name: "LEE", age: "21"}]; 
-=======
 'use strict';
 var _ = require('../../../node_modules/underscore');
 
 var _arr = [1, 2, 3, 4, 5, 6];
 var _2arr = [
+	0,
     [1, 2],
     [3, 4],
-    [6, 5]
+    [6, [5]]
 ];
 var _oarr = [{
     name: 'CHUI'
@@ -25,4 +19,13 @@ var _oarr = [{
     name: 'LEE',
     age: '21'
 }];
->>>>>>> 7e929c844e3a894b50cc855790264809ddeafa06
+
+// compact remove value == false;
+var _arrCompact = _.compact(_2arr);
+console.log(_arrCompact);
+
+// flatlen 
+var _arrFlatten = _.flatten(_2arr, true);
+console.log(_arrFlatten);
+
+// 
