7e929c844e3a894b50cc855790264809ddeafa06
Chuilee <chui.lee8@gmail.com>
2015-12-26 16:46:52 +0800

    array

---
+++ XTest/underscore/array.js
@@ -0,0 +1,20 @@
+'use strict';
+var _ = require('../../../node_modules/underscore');
+
+var _arr = [1, 2, 3, 4, 5, 6];
+var _2arr = [
+    [1, 2],
+    [3, 4],
+    [6, 5]
+];
+var _oarr = [{
+    name: 'CHUI'
+}, {
+    name: 'LEE'
+}, {
+    name: 'CHUI',
+    age: '20'
+}, {
+    name: 'LEE',
+    age: '21'
+}];
